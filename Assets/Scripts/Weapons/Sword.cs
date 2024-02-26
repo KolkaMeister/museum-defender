f28e7038f897cb7bc217b0b9fd183a412baec831
Subbota Mikhail <mihail.subbota@inbox.ru>
2024-02-26 11:13:44 +0300

    Non-taking away Sword

--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -1,26 +1,15 @@
 using System.Collections;
 using UnityEngine;
-using Zenject;
 
 public class Sword : MeleeWeapon
 {
     private static readonly int _attack = Animator.StringToHash("Attack");
     [SerializeField] private Animator _animator;
-    [SerializeField] private Collider2D _attackCollider;
-    
-    private Player _player;
-
-    [Inject]
-    public void Construct(Player player)
-    {
-        _player = player;
-    }
     
     public override void Attack()
     {
         if (!_fireCooldown.IsReady) return;
 
-        StartCoroutine(AnimationCoroutine());
         AnimateAttack();
         DealDamage();
         _fireCooldown.Reset();
@@ -29,18 +18,5 @@ public class Sword : MeleeWeapon
     protected override void AnimateAttack()
     {
         _animator.SetTrigger(_attack);
-    }
-
-    protected override IEnumerator AnimationCoroutine()
-    {
-        SetActiveAttack(true);
-        yield return new WaitForSeconds(animationTime);
-        SetActiveAttack(false);
-    }
-
-    private void SetActiveAttack(bool value)
-    {
-        _collider.enabled = !value;
-        _attackCollider.enabled = value;
     }
 }
