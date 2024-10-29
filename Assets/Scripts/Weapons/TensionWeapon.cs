1bd1d7553baffe3d2ffcddef87d61075ee2d8ec7
ivvanlyss <147874122+ivvanlyss@users.noreply.github.com>
2024-10-29 23:14:46 +0300

    oleg+vistrel

--- Assets/Scripts/Weapons/TensionWeapon.cs
+++ Assets/Scripts/Weapons/TensionWeapon.cs
@@ -6,17 +6,22 @@ public class TensionWeapon : RangeWeapon
 {
     [SerializeField] protected Transform _arrowHoldPoint;
     [SerializeField] protected Projectile _strungArrow;
+    [SerializeField] private AudioClip soundShoot;
+     private new AudioSource audio;
 
     private void Start()
     {
         if (_currentAmmo > 0)
             SetArrow();
+        audio = GetComponent<AudioSource>();
     }
     public override void Attack()
     {
         //Debug.Log("Attack");
         if (_strungArrow == null)
             return;
+        if (soundShoot)
+            audio.PlayOneShot(soundShoot);
         _strungArrow.transform.parent = null;
         _strungArrow.transform.localScale = Vector2.one;
         _strungArrow.AddForce((_arrowHoldPoint.position - _holdPoint.position).normalized, _force);
