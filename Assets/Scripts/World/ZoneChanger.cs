c937812b8e342fb1dec30cf1f737c203c80be10d
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2025-04-16 22:34:59 +0300

    portal to lvl2

---
+++ Assets/Scripts/World/ZoneChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.SceneManagement;
+
+public class ZoneChanger : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out PlayerInputController _))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
